2689f24c80ab1703a56fcdad1df3d94e16b21f20
Radomir Mijovic <radomir.mijovic@productdna.com>
2022-06-09 15:42:22 +0200

    feat: set navbar padding with clamp

--- components/navbar/NavbarStyled.js
+++ components/navbar/NavbarStyled.js
@@ -5,9 +5,18 @@ export const NavbarStyled = styled.nav`
   align-items: center;
   justify-content: space-around;
   width: 100%;
-  padding: 1rem clamp(1rem, 3vw, 8rem);
+  padding: 2rem clamp(1rem, 3vw, 8rem);
   
-  > h3 {
+  > h3, h1 {
     font-family: 'Josefin Sans', sans-serif;
+  }
+  
+  h1 {
+    text-transform: uppercase;
+    font-size: clamp(1.6rem, 3vw, 2rem);
+  }
+  
+  h3 {
+    font-size: clamp(1.2rem, 2vw, 1.4rem);
   }
 `
