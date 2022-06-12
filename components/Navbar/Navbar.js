4642e8579b2db3b3b9bcc3ee98e12c0e56b75a41
Radomir Mijovic <radomir.mijovic@productdna.com>
2022-06-12 15:34:56 +0200

    feat: added weather api for Budva

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import Image from "next/image";
+import React, {useState} from 'react';
 import {NavbarStyled} from "./NavbarStyled";
+import axios from "axios";
 
 const Navbar = () => {
+    const [temp, setTemp] = useState(0)
+
+    axios.get(
+        'https://api.openweathermap.org/data/2.5/weather?q=budva&units=metric&appid=cb5ff894f1a8e61d7e596c67a671568f'
+    ).then((response) => {
+        setTemp(response.data.main.temp)
+    })
+
     return (
         <NavbarStyled>
-            <h3>Budva 30C</h3>
+            <h3>Budva {temp.toFixed(0)}°C</h3>
             <h1>Balabusic</h1>
             <h3>info@balabusic.com</h3>
         </NavbarStyled>
